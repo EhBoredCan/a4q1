a2609ab26ab428a1abac25b42e41745b00c00b43
Senpai-Chans Yaoi PC <jesse.selin@sasktel.net>
2020-02-07 02:50:06 -0600

    Doc Strings and documentation for readers implemented. Program was given its finishing touches.

--- a4q1.py
+++ a4q1.py
@@ -1,11 +1,20 @@
 #  Jesse Selin jps496 11223887 CMPT145-02-L10
 
 def checkrow(main):
+    '''
+    Purpose: To check each row in the list of lists to see if its a latin square.
+    Pre-conditions: A list of lists with N sublists of N length.
+    Return: True if Latin SQ, False otherwise.
+    '''
+    # considered a latin square until proven otherwise
     latin = True
     n = len(main)
     for row in main:
+        # Used and altered for each row, if a True values remains after the row is iterated through then a value between
+        # 1 and n did not appear
         check = [True] * n
         for value in row:
+            # checks if incorrect values exist that cannot be in a latin square
             if value > n or value <= 0:
                 latin = False
                 break
@@ -17,14 +26,24 @@ def checkrow(main):
 
 
 def checkcol(main):
+    '''
+    Purpose: To check each column in the list of lists to see if its a latin square.
+    Pre-conditions: A list of lists with N sublists of N length.
+    Return: True if Latin SQ, False otherwise.
+    '''
+    # considered a latin square until proven otherwise
     latin = True
     n = len(main)
     for column in range(len(main)):
+        # Used and altered for each column, if a True values remains after the column is iterated through then a value
+        # between 1 and n did not appear
         check = [True] * n
         for row in range(n):
+            # checks if incorrect values exist that cannot be in a latin square
             if main[row] == [] or main[column][row] > n or main[row][column] <= 0:
                 latin = False
                 break
+            # column is held while each each row in that column is iterated through
             check[main[row][column] - 1] = False
         if True in check:
             latin = False
@@ -33,6 +52,12 @@ def checkcol(main):
 
 
 def checker(main):
+    '''
+    Purpose: To check the square is a latin square with checkrow/checkcol.
+    Pre-conditions: A list of lists with N sublists of N length. Function checkrow and checkcol exist.
+    Post-conditons: Return values from checkrow and checkcol.
+    Return: Prints Yes if Latin SQ, No if not.
+    '''
     if checkcol(main) and checkrow(main):
         print('Yes')
     else:
@@ -40,9 +65,16 @@ def checker(main):
 
 
 def read(filename):
+    '''
+    Purpose: To open the file with the filename
+    Pre-conditions: A filename as a string
+    Post-conditons: File is read and creates a list of lists of integers.
+    Return: A list of lists with N sublists of N length.
+    '''
     file = open(filename, 'r')
     unorganizedvalues = [x.split() for x in file]
     n = int(unorganizedvalues[0][0])
+    # a main list is created then the unorganized values from line 2 to N is set into sublists appended to the main list
     main = []
     row = 1
     while row < n + 1:
@@ -55,6 +87,12 @@ def read(filename):
 
 
 def ask():
+    '''
+    Purpose: Asks for the name of a file from console using input.
+    Pre-conditions: The filename is written correctly.
+    Post-conditons: The filename is a string.
+    Return: A filename set as a string.
+    '''
     value = str(input('Please enter a filename for your Latin Square test:'))
     return value
 
