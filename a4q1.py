cceac3325c0d2905c8456c9aa729f28cec9ac2f3
Senpai-Chans Yaoi PC <jesse.selin@sasktel.net>
2020-02-07 00:35:36 -0600

    Checkrow had an error since the value would go out of the lists index since it started at 1 not 0. Set an if statement to catch values greater then n and 0 and less.

--- a4q1.py
+++ a4q1.py
@@ -5,7 +5,10 @@ def checkrow(main, n):
     for row in main:
         check = [True] * n
         for value in row:
-            check[value] = False
+            if value > n or value <= 0:
+                latin = False
+                break
+            check[value-1] = False
         if True in check:
             latin = False
             break
--- a4q1_testing.py
+++ a4q1_testing.py
@@ -12,36 +12,36 @@ test = [[1, 2, 3], [1, 2, 3], [1, 2, 3]]
 reason = 'Three N = 3 size sublists with correct row values'
 result = a1.checkrow(test,3)
 if result != True:
-    print(reason, 'should return True returned ', result)
+    print(reason, 'should return True returned', result)
 
 test = [[1]]
 reason = 'A list of 1'
 result = a1.checkrow(test,1)
 if result != True:
-    print(reason, 'should return True returned ', result)
+    print(reason, 'should return True returned', result)
 
 test = [[1, 4, 3], [1, 2, 3], [3, 2, 1]]
 reason = 'Incorrect sublist in the front and reversed value at the end'
 result = a1.checkrow(test,3)
 if result != False:
-    print(reason, 'should return False returned ', result)
+    print(reason, 'should return False returned', result)
 
 test = [[3, 2, 1], [1, 4, 3], [1, 2, 3]]
 reason = 'Incorrect sublist in the middle and reversed value in the front'
 result = a1.checkrow(test,3)
 if result != False:
-    print(reason, 'should return False returned ', result)
+    print(reason, 'should return False returned', result)
 
 test = [[1, 2, 3], [3, 2, 1], [1, 4, 3]]
 reason = 'Incorrect sublist at the end and reversed value in the middle'
 result = a1.checkrow(test,3)
 if result != False:
-    print(reason, 'should return False returned ', result)
+    print(reason, 'should return False returned', result)
 
 test = [[]]
 reason = 'An empty sublist'
 result = a1.checkrow(test,1)
-if result != True:
-    print(reason, 'should return True returned ', result)
+if result != False:
+    print(reason, 'should return True returned', result)
 
 print('Tests Complete')
